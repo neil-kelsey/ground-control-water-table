839496b9fcb832f54fa7b13824a580e24e7d6f20
Neil Kelsey <neil.kelsey@gmail.com>
2024-05-05 18:19:53 +0100

    feat: I think the table functionality is complete even if it does look awful so Im moving on to the map functionality and adding a simple page - you can now find this page at http://localhost:3000/map

---
+++ ground-control-water-table/src/app/map/page.tsx
@@ -0,0 +1,13 @@
+"use client"; // This is a client component
+
+import React from 'react';
+
+const MapPage = () => {
+    return (
+        <div>
+            <h1>Map page</h1>
+        </div>
+    );
+};
+
+export default MapPage;
