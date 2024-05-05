b84ff626ef0144afa7edd19fc33f580db7b2fd1e
Neil Kelsey <neil.kelsey@gmail.com>
2024-05-05 14:49:24 +0100

    feat: Adding basic table component layout which just dumps out the data in a basic table layout, pretty slow and clunky at this stage as its 10000 data items, display two rows of data per item - so this is a table with 20000 rows!

--- ground-control-water-table/src/app/components/table.tsx
+++ ground-control-water-table/src/app/components/table.tsx
@@ -2,12 +2,43 @@
 
 import React from 'react';
 
-const TableComponent = () => {
-    return (
-        <div>
-            <p>Table component</p>
-        </div>
-    );
+const TableComponent  = ({ data }) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Location</th>
+            <th>Sensor ID</th>
+            <th>Show details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <>
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.latitude}, {item.longitude}</td>
+                <td>{item.sensorId}</td>
+                <td>
+                  <p>Show details</p>
+                </td>
+              </tr>
+              {Object.entries(item.details).map(([key, value]) => (
+                <tr>
+                  <td colSpan={4} key={key}>
+                    <strong>{key}:</strong> 
+                    {typeof value === 'object' ? JSON.stringify(value) : value}
+                  </td>
+                </tr>
+              ))}
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-export default TableComponent;
+export default TableComponent ;
--- ground-control-water-table/src/app/table/page.tsx
+++ ground-control-water-table/src/app/table/page.tsx
@@ -1,20 +1,22 @@
 "use client"; // This is a client component
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import TableComponent from '../components/table';
 import riverSensorData from '../data/river_sensor_data.json';
 import { decodeAndMutateData } from '../functions/decodeAndMutateData';
 
 const TablePage = () => {
+    const [data, setData] = useState()
     useEffect(() => {
         const data = decodeAndMutateData(riverSensorData);
         console.log('NeilTest - data', data);
+        setData(data)
     }, []); // Empty dependency array to run the effect only once
 
     return (
         <div>
             <h1>Table page</h1>
-            <TableComponent />
+            {data ? <TableComponent data={data} /> : <div>No data / loading component</div>}
         </div>
     );
 };
