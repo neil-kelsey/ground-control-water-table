ffbeb57f03870c09f50466d1679f87595444e4cb
Neil Kelsey <neil.kelsey@gmail.com>
2024-05-05 14:20:43 +0100

    feat: Adding basic table page - you can view this at http://localhost:3000/table

---
+++ ground-control-water-table/src/app/table/page.tsx
@@ -0,0 +1,13 @@
+"use client"; // This is a client component
+
+import React from 'react';
+
+const TablePage = () => {
+    return (
+        <div>
+            <h1>Table</h1>
+        </div>
+    );
+};
+
+export default TablePage;
