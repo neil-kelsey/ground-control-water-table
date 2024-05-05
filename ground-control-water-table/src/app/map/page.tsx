423bb96921d4f8c302384e8aa150ccb097041aeb
Neil Kelsey <neil.kelsey@gmail.com>
2024-05-05 19:07:42 +0100

    feat: Creating a basic Mapbox working example - just a map with nothing on it yet

--- ground-control-water-table/src/app/map/page.tsx
+++ ground-control-water-table/src/app/map/page.tsx
@@ -1,13 +1,26 @@
 "use client"; // This is a client component
 
-import React from 'react';
+import { useState, useRef } from "react";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import classes from "./Page.modules.css";
 
-const MapPage = () => {
-    return (
-        <div>
+export default function MapPage() {
+	const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+	const mapRef = useRef(null);
+	return (
+		<main className="map-wrapper">
             <h1>Map page</h1>
-        </div>
-    );
-};
-
-export default MapPage;
+			<Map
+                ref={mapRef}
+				mapboxAccessToken={mapboxToken}
+				mapStyle="mapbox://styles/mapbox/streets-v12"
+                style={classes.mapStyle}
+				initialViewState={{ latitude: 51.736449, longitude: -4.049007, zoom: 10 }}
+				maxZoom={20}
+				minZoom={3}
+			>
+            </Map>
+		</main>
+	);
+}
