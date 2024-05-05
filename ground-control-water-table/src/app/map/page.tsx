a44bfdfaf3bc43729864882289eff7c2e6c94637
Neil Kelsey <neil.kelsey@gmail.com>
2024-05-05 19:33:39 +0100

    feat: Adding custom data to the map - at the moment just some airport data from a tutorial Im following - look in Japan for lots of points of data

--- ground-control-water-table/src/app/map/page.tsx
+++ ground-control-water-table/src/app/map/page.tsx
@@ -1,16 +1,21 @@
 "use client"; // This is a client component
 
 import { useState, useRef } from "react";
-import Map, { NavigationControl, GeolocateControl } from "react-map-gl";
+import Map, { Marker, Popup, NavigationControl, GeolocateControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import classes from "./Page.modules.css";
+import airports from "../data/airports.json";
+import MapMarker from "../components/mapMarker";
 
 export default function MapPage() {
 	const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+	const [selectedMarker, setSelectedMarker] = useState(null);
 	const mapRef = useRef(null);
+
 	return (
 		<main className="map-wrapper">
             <h1>Map page</h1>
+            <MapMarker />
 			<Map
                 ref={mapRef}
 				mapboxAccessToken={mapboxToken}
@@ -22,6 +27,13 @@ export default function MapPage() {
 			>
                 <GeolocateControl position="top-left" />
 				<NavigationControl position="top-left" />
+                {airports.map((airport, index) => {
+					return (
+						<Marker key={index} longitude={airport.lon} latitude={airport.lat}>
+							<MapMarker size={30} color="tomato" />
+						</Marker>
+					);
+				})}
             </Map>
 		</main>
 	);
